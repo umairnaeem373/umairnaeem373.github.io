0ddcb66bab338b48ad98152029beae06c658b503
umairnaeem373 <umairnaeem373@gmail.com>
2024-08-22 18:56:03 +0500

    Project s Updated

--- src/content_option.js
+++ src/content_option.js
@@ -1,114 +1,131 @@
 const logotext = "Mʌʀɩ";
 const meta = {
-    title: "Umair Naeem",
-    description: "I’m M.Umair Naeem,a MERN stack developer,currently working in Lahore",
+  title: "Umair Naeem",
+  description:
+    "I’m M.Umair Naeem,a MERN stack developer,currently working in Lahore",
 };
 
 const introdata = {
-    title: "I’m Mʌʀɩ Bʌɓʌ",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop web apps",
-    },
-    description: "Explore my portfolio showcasing diverse projects and skills in web development. Let's create innovative solutions together. Welcome to my site",
-    your_img_url: "https://www.facebook.com/share/ekoyMUaQwTtirBLg/",
+  title: "I’m Mʌʀɩ Bʌɓʌ",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop web apps",
+  },
+  description:
+    "Explore my portfolio showcasing diverse projects and skills in web development. Let's create innovative solutions together. Welcome to my site",
+  your_img_url: "../../assets/images/profile.jpg",
 };
 
 const dataabout = {
-    title: "A bit about my self",
-    aboutme: "Welcome to my portfolio! I'm a MERN stack web developer passionate about crafting intuitive and efficient web solutions. Explore my projects and skills to learn more about my work.",
+  title: "A bit about my self",
+  aboutme:
+    "Welcome to my portfolio! I'm a MERN stack web developer passionate about crafting intuitive and efficient web solutions. Explore my projects and skills to learn more about my work.",
 };
-const worktimeline = [{
+const worktimeline = [
+  {
     jobtitle: "Fron End Developer",
     where: "VCT",
     date: "2023",
-},
+  },
 ];
 
-const skills = [{
+const skills = [
+  {
     name: "Html",
     value: 90,
-},
-{
+  },
+  {
     name: "Css",
     value: 85,
-},
-{
+  },
+  {
     name: "Javascript",
     value: 70,
-},
-{
+  },
+  {
     name: "Reactjs",
     value: 75,
-},
-{
+  },
+  {
     name: "Nodejs",
     value: 50,
-},
-{
+  },
+  {
     name: "Expressjs",
     value: 80,
-},
-{
+  },
+  {
     name: "MongoDB",
     value: 80,
-},
+  },
 ];
 
 const services = [
-    {
-        title: "Web Apps",
-        description: "We offer custom web development, responsive design, e-commerce solutions, SEO optimization, and ongoing support to help your business thrive online.",
-    },
-    {
-        title: "Web Design",
-        description: "We specialize in creating custom web designs that are modern, user-friendly, responsive, and tailored to showcase your brand effectively online.",
-    },
+  {
+    title: "Web Apps",
+    description:
+      "We offer custom web development, responsive design, e-commerce solutions, SEO optimization, and ongoing support to help your business thrive online.",
+  },
+  {
+    title: "Web Design",
+    description:
+      "We specialize in creating custom web designs that are modern, user-friendly, responsive, and tailored to showcase your brand effectively online.",
+  },
 ];
 
-const dataportfolio = [{
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "E-Commerce Website-Amazona",
+
+    link: "https://umairnaeem373.github.io/E-commerce-amazona/",
+  },
+  {
     img: "https://picsum.photos/400/?grayscale",
     description: "Dev Connections",
     link: "https://umairnaeem373.github.io/dev-connections-redux/",
-},
-{
+  },
+  {
     img: "https://picsum.photos/400/800/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
     link: "#",
-},
-{
+  },
+  {
     img: "https://picsum.photos/400/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
     link: "#",
-},
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "umairnaeem373@gmail.com",
-    YOUR_FONE: "0334 1041181",
-    description: "Please feel free to reach out using the form below. Your message is important to us and we'll respond promptly. Thank you for contacting us!. ",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_y8ourg9",
-    YOUR_TEMPLATE_ID: "template_blg9uew",
-    YOUR_USER_ID: "y4D7K_xH4zzNDR1gi",
+  YOUR_EMAIL: "umairnaeem373@gmail.com",
+  YOUR_FONE: "0334 1041181",
+  description:
+    "Please feel free to reach out using the form below. Your message is important to us and we'll respond promptly. Thank you for contacting us!. ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_y8ourg9",
+  YOUR_TEMPLATE_ID: "template_blg9uew",
+  YOUR_USER_ID: "y4D7K_xH4zzNDR1gi",
 };
 
 const socialprofils = {
-    github: "https://github.com/umairnaeem373",
-    facebook: "https://www.facebook.com/umairnaeem373",
-    linkedin: "https://www.linkedin.com/in/umairnaeem373",
+  github: "https://github.com/umairnaeem373",
+  facebook: "https://www.facebook.com/umairnaeem373",
+  linkedin: "https://www.linkedin.com/in/umairnaeem373",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Typewriter from "typewriter-effect";
 import { introdata, meta } from "../../content_option";
 import { Link } from "react-router-dom";
-
+import profilePic from '../../assets/images/profile.jpg'
 export const Home = () => {
   return (
     <HelmetProvider>
@@ -17,8 +17,10 @@ export const Home = () => {
         <div className="intro_sec d-block d-lg-flex align-items-center ">
           <div
             className="h_bg-image order-1 order-lg-2 h-100"
-            style={{ backgroundImage: `url(${introdata.your_img_url})` }}
-          ></div>
+            style={{ backgroundImage: `url(${profilePic})` }}
+          >
+            <img src=""/>
+          </div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
               <div className="intro mx-auto">
